8ddfad4b8833298e62a6bf19e9677e53b053f69a
Rachael Morris <rachael.morris@sky.uk>
2021-05-19 14:39:15 +0100

    Add background and reset styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from "react";
 import "./styles/main.scss";
 
 const App: FunctionComponent = () => {
-  return <div className="App"></div>;
+  return <div className="app"></div>;
 };
 
 export default App;
